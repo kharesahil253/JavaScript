5e1d3b1693b3357ae1686d58c017a4bbc75511b7
Sahil Khare <110685756+kharesahil253@users.noreply.github.com>
2025-03-17 21:29:28 +0530

    code commit

--- 01_lecture/index.js
+++ 01_lecture/index.js
@@ -1,6 +1,6 @@
 console.log("Hello World");
-console.log("*****");
-console.log("*****");
+console.log("*");
+console.log("***");
 console.log("*****");
 console.log("*****");
 console.log("*****");
