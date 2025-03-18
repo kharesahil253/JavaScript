a87887aa712583a1f7e34c01ed1f6cb3e690d587
Sahil Khare <110685756+kharesahil253@users.noreply.github.com>
2025-03-18 18:57:56 +0530

    SEconf com

---
+++ 02_lecture/index2.js
@@ -0,0 +1,54 @@
+// let fname='Sahil';
+// let lname='Khare';
+// console.log("Sahil");
+// console.table(fname,lname);
+// console.warn("Sahil");
+// console.error("Sahil");
+// var fname ="Sahil";
+// var number= 31;
+var isTrue= true;
+// console.log(fname,number,isTrue);
+// var num=10;
+// {
+//     var num=20;
+//     console.log(num);
+// }
+// console.log(num);
+// var num=10;
+// function fun(){
+//     console.log(num);
+//     var num=20;
+// }
+// fun();
+// console.log(num);
+// let num = 10;
+// {
+//     let num = 20;
+//     console.log(num);
+// }
+// console.log(num);
+// const lname="Dog"
+// console.log(lname)
+// lname ="dhruv"
+//  Primitve data type Stack
+//NON heap
+//num string bool undefined symbol bigint null 
+// const sav=null
+// console.log(sav)
+// var num2=10;
+// console.log(typeof num2)
+// console.log(typeof num)
+// console.log(typeof sav)
+// console.log(typeof lname)
+// console.log(typeof isTrue)
+
+//non primitve array date 
+// let a= 20;
+// console.log(a)
+// {
+//     a=30
+//     console.log(a)
+// }
+// console.log(a)
+num=20
+console.log(typeof num)
