05c06315a24823345e30a765fabd0ad5d458c281
Sahil Khare <110685756+kharesahil253@users.noreply.github.com>
2025-03-21 21:52:17 +0530

    modif

--- 04_lecture/Practice.js
+++ 04_lecture/Practice.js
@@ -1,62 +1,72 @@
-// nested if-else statement☑️
-let Temp = 35;
+// nested if-else statement
+let Temp = 28;
 let humid = 75;
-let wind = 10 ;
+let wind = 10;
 
 if (Temp >= 35) {
     console.log("It's very hot! Drink water.")
+}
+else {
     if (Temp >= 25 && Temp < 35) {
-        console.log("  ")
-    }
-    else {
         if (humid > 70) {
             console.log("Hot and humid, turn on the AC.")
-        }
-        else {
+
+        } else {
             console.log("It's warm but manageable.")
         }
+    }
+    if (Temp >= 15 && Temp < 25) {
         if (wind > 15) {
-            console.log("Cool breaze is blowing")
+            console.log("Cool breeze is blowing.")
+
         } else {
-            console.log("The Weather is pleasant")
+            console.log("The weather is pleasant.")
         }
+    }
+    else if (Temp < 15) {
+        console.log("It's cold, wear a sweater.")
     }
 }
 
 // use nested ternary☑️
-let price = 2300;
+let price = 8000;
 let membership = true
-price >= 10000 ? console.log("20% discount") : price >= 5000 && price < 10000 ? console.log("10% discount") : price < 5000 && (membership === true) ? console.log("5% discount") : console.log("no discount")
+let disc = price >= 10000 ? 20 : price >= 5000 && price < 10000 ? 10 : price < 5000 && (membership === true) ? 5 : 0
+console.log(`Discount ${disc}`)
 
-// if-else + ternary☑️
-let marks = 45
-let attendance = 75
+// if-else + ternary
+let marks = 75
+let attendance = 85
 if (marks >= 90) {
     console.log("A+")
 } else {
-    marks >= 80 ? console.log("A") : marks >= 70 && attendance > 75 ? console.log("B+") : console.log("B")
-    if (60 >= marks) {
-        console.log("C")
-    } else {
-        clg("Fail")
+    if (marks >= 80) {
+        console.log("A")
+    }
+    else {
+        if (marks >= 70) {
+            attendance > 75 ? console.log("B+") : console.log("B")
+        }
+        else {
+            marks >= 60 ? console.log("C") : console.log("Fail")
+        }
     }
 }
 
-//Switch-case + ternary
-// let food="pizza"
-// let size = "small";
-// switch (size) {
-//     case small: console.log("$200")
-//         break;
-//     case medium: console.log("$400")
-//         break;
-//     case large: console.log("$600")
-//         break;
-//     case 
-//     default:
-//         food = "Burger" ? console.log("$150") : console.log("300")
-//         break;
-// }
+// Switch-case + ternary
+let food = "pizza"
+let size = "large";
+switch (food) {
+    case "pizza": size == 'small' ? console.log("$200") : size === 'medium' ? console.log("$400") : console.log("$600")
+        break;
+    case "Burger": console.log("$150")
+        break;
+    case "Pasta": console.log("300")
+        break;
+    default:
+        console.log("Not Available")
+
+}
 
 //Nested ternary☑️
 let age = 35
@@ -79,17 +89,17 @@ switch (vechile) {
 }
 
 //else if ☑️
-let Marks = 96
-let Attendance = 95
+let Marks = 70
+let Attendance = 65
 if (Marks >= 80 && Attendance >= 75) {
     console.log("Excellent")
-} 
-else if(Marks >= 60 && Attendance >= 60 ) {
+}
+else if (Marks >= 60 && Attendance >= 60) {
     console.log("Good")
 }
-else if(Marks >= 40 && Attendance >= 50){
+else if (Marks >= 40 && Attendance >= 50) {
     console.log("Average")
 }
-else{
+else {
     console.log("Poor")
 }
